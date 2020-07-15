820d39c5edbff50e65f165b31262c24bf08c109a
Mahima Srivastava <mahima5srivastava@gmail.com>
2020-07-15 18:03:21 +0530

    Sending Photo to previous controller with RxSwift

--- CameraFilter/CameraFilter/ViewController.swift
+++ CameraFilter/CameraFilter/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - IBOutlet
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    //MARK: - Properties
+    let disposeBag = DisposeBag()
+    
+    //MARK:- Overridden Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let navVc = segue.destination as? UINavigationController,
+            let photoVc = navVc.viewControllers.first as? PhotosCollectionViewController {
+            photoVc.selectedPhoto.subscribe (onNext: {[weak self] photo in
+                self?.photoImageView.image = photo
+                }).disposed(by: disposeBag)
+        }
+    }
 }
 
